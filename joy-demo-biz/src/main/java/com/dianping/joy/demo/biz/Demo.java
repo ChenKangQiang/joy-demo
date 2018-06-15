ca9ee9f9e7b32bf872376bd98c29489087edd224
chenkangqiang <chenkangqiang@meituan.com>
2018-06-15 16:12:47 +0800

    ### mvc

---
+++ joy-demo-biz/src/main/java/com/dianping/joy/demo/biz/Demo.java
@@ -0,0 +1,31 @@
+package com.dianping.joy.demo.biz;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import lombok.Data;
+import org.apache.commons.text.StringEscapeUtils;
+
+
+/**
+ * @Description:
+ * @Author: chenkangqiang
+ * @Date: 2018/6/15
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println(StringEscapeUtils.escapeJava("哈哈"));
+        System.out.println("\\u54C8\\u54C8");
+
+        System.out.println(JSON.toJSONString(new Dog(), SerializerFeature.BrowserCompatible));
+        System.out.println(JSON.toJSONString(new Dog()));
+    }
+
+    @Data
+    public static class Dog {
+        String name = StringEscapeUtils.escapeJava("哈哈");
+        String nickName = "\u54C8\u54C8";
+    }
+
+}
